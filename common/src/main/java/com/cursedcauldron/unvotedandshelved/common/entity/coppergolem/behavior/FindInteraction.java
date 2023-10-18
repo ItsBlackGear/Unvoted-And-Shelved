70a1e2896b7b5a908d03da1d773a8d807e5ce0ba
ItsBlackGear <itsblackgear@gmail.com>
2023-10-18 20:27:11 -0300

    🐛 Check for button availability
    - Copper Golems will now check if a button can be pressed before interacting
    - Added Copper Button click sound on interaction

--- common/src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/coppergolem/behavior/FindInteraction.java
+++ common/src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/coppergolem/behavior/FindInteraction.java
@@ -9,6 +9,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.behavior.Behavior;
 import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
@@ -16,15 +17,26 @@ import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.Path;
 import org.apache.commons.compress.utils.Lists;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+/**
+ * The Copper Golem is programmed to find a suitable location for interaction within a specified radius.
+ * It searches for locations where it can interact with blocks. Once a location is found,
+ * the Copper Golem moves to that location and performs the interaction.
+ * <p>
+ * The behavior includes checking if the Copper Golem already has a suitable interaction location in memory,
+ * If it doesn't it searches for a new location and ensures it reaches that location by setting a walking
+ * target. If the Golem can't reach the location, it will look for an alternative nearby location.
+ * If no suitable locations are found, the Golem resets its memory and enters a cooldown period.
+ */
 public class FindInteraction extends Behavior<CopperGolem> {
     private static final IntProvider INTERACTION_COOLDOWN = UniformInt.of(120, 240);
     private final int searchRadius;
 
-    private BlockPos copperPos;
-    private BlockPos copperPosBelow;
+    private BlockPos interactionPosAbove;
+    private BlockPos interactionPosBelow;
 
     public FindInteraction(int searchRadius) {
         super(ImmutableMap.of(
@@ -35,65 +47,100 @@ public class FindInteraction extends Behavior<CopperGolem> {
         this.searchRadius = searchRadius;
     }
 
+    /**
+     * Checks if the Copper Golem has no interaction memory, which includes information about its
+     * current interaction location and cooldown.
+     */
     @Override
     protected boolean checkExtraStartConditions(ServerLevel level, CopperGolem golem) {
-        return golem.getBrain().getMemory(USMemoryModules.INTERACTION_POS.get()).isEmpty() && golem.getBrain().getMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get()).isEmpty();
+        return this.hasNoInteractionMemory(golem);
     }
 
+    /**
+     * Checks if the Copper Golem can continue to use this behavior, which depends on whether it has
+     * interaction memory or not.
+     */
     @Override
     protected boolean canStillUse(ServerLevel level, CopperGolem golem, long gameTime) {
-        return golem.getBrain().getMemory(USMemoryModules.INTERACTION_POS.get()).isEmpty() && golem.getBrain().getMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get()).isEmpty();
+        return this.hasNoInteractionMemory(golem);
     }
 
+    /**
+     * Attempts to find a suitable iteration position and sets up the locations for interaction
+     * above and below the found position.
+     */
     @Override
     protected void start(ServerLevel level, CopperGolem golem, long gameTime) {
-        BlockPos copperPos = this.getCopperPos(level, golem);
-        if (copperPos != null) {
-            this.copperPos = copperPos;
-            this.copperPosBelow = copperPos.below();
+        BlockPos interactionPosition = this.getInteractionPosition(level, golem);
+        if (interactionPosition != null) {
+            this.interactionPosAbove = interactionPosition;
+            this.interactionPosBelow = interactionPosition.below();
         }
     }
 
+    /**
+     * Handles the movement and path creation to the interaction locations above and below.
+     * If the Golem can't reach the locations, it resets its memory.
+     */
     @Override
     protected void tick(ServerLevel level, CopperGolem golem, long gameTime) {
-        BlockPos copperPos = this.copperPos;
-        BlockPos copperPosBelow = this.copperPosBelow;
-        if (copperPos != null && copperPosBelow != null) {
-            BehaviorUtils.setWalkAndLookTargetMemories(golem, copperPos, 0.4F, 1);
-            Path button = golem.getNavigation().createPath(copperPos, 1);
-            Path buttonBelow = golem.getNavigation().createPath(copperPosBelow, 1);
+        BlockPos interactionPosAbove = this.interactionPosAbove;
+        BlockPos interactionPosBelow = this.interactionPosBelow;
+        if (interactionPosAbove != null && interactionPosBelow != null) {
+            // Set the walking and looking target for the golem
+            BehaviorUtils.setWalkAndLookTargetMemories(golem, interactionPosAbove, 0.4F, 1);
 
-            if (button != null && button.canReach()) {
-                golem.getNavigation().moveTo(button, 0.4);
-                BlockState copperState = level.getBlockState(copperPos);
-                if (golem.blockPosition().closerThan(copperPos, 2) && copperState.is(USBlockTags.COPPER_GOLEM_INTERACTABLES)) {
-                    golem.getBrain().setMemory(USMemoryModules.INTERACTION_POS.get(), copperPos);
-                    this.copperPos = copperPos;
-                }
-            } else if (buttonBelow != null && buttonBelow.canReach()) {
-                golem.getNavigation().moveTo(buttonBelow, 0.4);
-                BlockState copperBelowState = level.getBlockState(copperPosBelow);
-                if (golem.blockPosition().closerThan(copperPosBelow, 2) && copperBelowState.is(USBlockTags.COPPER_GOLEM_INTERACTABLES)) {
-                    golem.getBrain().setMemory(USMemoryModules.INTERACTION_POS.get(), copperPosBelow);
-                    this.copperPosBelow = copperPosBelow;
-                }
+            // Create paths for both interaction positions
+            Path pathAbove = golem.getNavigation().createPath(interactionPosAbove, 1);
+            Path pathBelow = golem.getNavigation().createPath(interactionPosBelow, 1);
+
+            // Check if the Copper Golem can reach any of the interaction positions.
+            if (pathAbove != null && pathAbove.canReach()) {
+                this.interactionPosAbove = this.calculatePosition(level, golem, interactionPosAbove, pathAbove);
+            } else if (pathBelow != null && pathBelow.canReach()) {
+                this.interactionPosBelow = this.calculatePosition(level, golem, interactionPosBelow, pathBelow);
             } else {
-                golem.getBrain().eraseMemory(USMemoryModules.INTERACTION_POS.get());
-                golem.getBrain().setMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get(), INTERACTION_COOLDOWN.sample(level.random));
+                // Reset memory and set a cooldown period
+                this.resetInteractions(golem.getBrain(), level.random);
             }
         }
     }
 
-    private BlockPos getCopperPos(ServerLevel level, CopperGolem golem) {
+    /**
+     * Calculates the interaction position for the Copper Golem based on a provided path.
+     * It moves the Golem to the path and checks if it's near an interactable block.
+     */
+    private @Nullable BlockPos calculatePosition(ServerLevel level, CopperGolem golem, BlockPos pos, Path path) {
+        // Move the golem to the path and check if it's near an interactable block
+        golem.getNavigation().moveTo(path, 0.4);
+        BlockState state = level.getBlockState(pos);
+        if (golem.blockPosition().closerThan(pos, 2) && state.is(USBlockTags.COPPER_GOLEM_INTERACTABLES)) {
+            golem.getBrain().setMemory(USMemoryModules.INTERACTION_POS.get(), pos);
+            return pos;
+        }
+
+        return null;
+    }
+
+    /**
+     * Determines a suitable interaction position for the Copper Golem within its search radius.
+     * It checks nearby positions for interactable blocks.
+     */
+    private BlockPos getInteractionPosition(ServerLevel level, CopperGolem golem) {
         int radius = this.searchRadius;
         RandomSource random = level.random;
 
+        // Create a list to store possible interaction positions.
         List<BlockPos> possibles = Lists.newArrayList();
+
+        // Iterate over positions within the specified search radius.
         for (int x = -radius; x <= radius; x++) {
             for (int y = -radius; y <= radius; y++) {
                 for (int z = -radius; z <= radius; z++) {
                     BlockPos pos = new BlockPos(golem.getX() + x, golem.getY() + y, golem.getZ() + z);
                     BlockState state = level.getBlockState(pos);
+
+                    // Check if the block at this position is interactable
                     if (state.is(USBlockTags.COPPER_GOLEM_INTERACTABLES)) {
                         possibles.add(pos);
                     }
@@ -102,10 +149,29 @@ public class FindInteraction extends Behavior<CopperGolem> {
         }
 
         if (possibles.isEmpty()) {
-            golem.getBrain().setMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get(), INTERACTION_COOLDOWN.sample(random));
+            // No suitable interactable blocks found, set a cooldown period
+            this.resetInteractions(golem.getBrain(), random);
             return null;
         } else {
+            // Return a random interactable position
             return possibles.get(random.nextInt(possibles.size()));
         }
+    }
+
+    /**
+     * Checks if the Copper Golem has no interaction memory, which includes information about its
+     * current interaction location and cooldown.
+     */
+    private boolean hasNoInteractionMemory(CopperGolem golem) {
+        return golem.getBrain().getMemory(USMemoryModules.INTERACTION_POS.get()).isEmpty() &&
+                golem.getBrain().getMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get()).isEmpty();
+    }
+
+    /**
+     * Resets the Copper Golem's interaction memories and sets a new cooldown period.
+     */
+    private void resetInteractions(Brain<CopperGolem> brain, RandomSource random) {
+        brain.eraseMemory(USMemoryModules.INTERACTION_POS.get());
+        brain.setMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get(), INTERACTION_COOLDOWN.sample(random));
     }
 }
--- common/src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/coppergolem/behavior/TriggerInteraction.java
+++ common/src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/coppergolem/behavior/TriggerInteraction.java
@@ -2,14 +2,17 @@ package com.cursedcauldron.unvotedandshelved.common.entity.coppergolem.behavior;
 
 import com.cursedcauldron.unvotedandshelved.common.entity.USPoses;
 import com.cursedcauldron.unvotedandshelved.common.entity.coppergolem.CopperGolem;
+import com.cursedcauldron.unvotedandshelved.common.registries.USSoundEvents;
 import com.cursedcauldron.unvotedandshelved.common.registries.entity.USMemoryModules;
 import com.cursedcauldron.unvotedandshelved.core.data.tags.USBlockTags;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.Pose;
@@ -19,7 +22,6 @@ import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.level.block.ButtonBlock;
 import net.minecraft.world.level.block.LeverBlock;
-import net.minecraft.world.level.block.WoodButtonBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -28,7 +30,7 @@ import java.util.Optional;
 
 public class TriggerInteraction extends Behavior<CopperGolem> {
     private static final IntProvider INTERACTION_COOLDOWN = UniformInt.of(120, 240);
-    private int buttonTicks;
+    private int interactionTicks;
 
     public TriggerInteraction() {
         super(ImmutableMap.of(
@@ -39,12 +41,12 @@ public class TriggerInteraction extends Behavior<CopperGolem> {
 
     @Override
     protected boolean checkExtraStartConditions(ServerLevel level, CopperGolem golem) {
-        return golem.getBrain().getMemory(USMemoryModules.INTERACTION_POS.get()).isPresent() && golem.getBrain().getMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get()).isEmpty();
+        return this.canInteractWithPosition(golem);
     }
 
     @Override
     protected boolean canStillUse(ServerLevel level, CopperGolem golem, long gameTime) {
-        return golem.getBrain().getMemory(USMemoryModules.INTERACTION_POS.get()).isPresent() && golem.getBrain().getMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get()).isEmpty();
+        return this.canInteractWithPosition(golem);
     }
 
     @Override
@@ -74,25 +76,10 @@ public class TriggerInteraction extends Behavior<CopperGolem> {
         });
     }
 
-    private void triggerInteraction(ServerLevel level, BlockPos pos, BlockState state) {
-        if (state.getBlock() instanceof ButtonBlock button) {
-            button.press(state, level, pos);
-
-            if (button instanceof WoodButtonBlock) {
-                level.playSound(null, pos, SoundEvents.WOODEN_BUTTON_CLICK_ON, SoundSource.BLOCKS, 0.3F, 0.6F);
-            } else {
-                level.playSound(null, pos, SoundEvents.STONE_BUTTON_CLICK_ON, SoundSource.BLOCKS, 0.3F, 0.6F);
-            }
-        } else if (state.getBlock() instanceof LeverBlock lever) {
-            lever.pull(state, level, pos);
-            level.playSound(null, pos, SoundEvents.LEVER_CLICK, SoundSource.BLOCKS, 0.3F, 0.6F);
-        }
-    }
-
     @Override
     protected void tick(ServerLevel level, CopperGolem golem, long gameTime) {
-        if (this.buttonTicks < 60) {
-            this.buttonTicks++;
+        if (this.interactionTicks < 60) {
+            this.interactionTicks++;
         } else {
             golem.getBrain().eraseMemory(USMemoryModules.INTERACTION_POS.get());
             golem.getBrain().setMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get(), INTERACTION_COOLDOWN.sample(level.getRandom()));
@@ -102,9 +89,36 @@ public class TriggerInteraction extends Behavior<CopperGolem> {
 
     @Override
     protected void stop(ServerLevel level, CopperGolem golem, long gameTime) {
-        if (this.buttonTicks >= 1) {
-            this.buttonTicks = 0;
+        if (this.interactionTicks >= 1) {
+            this.interactionTicks = 0;
             golem.setPose(Pose.STANDING);
         }
+    }
+
+    private void triggerInteraction(ServerLevel level, BlockPos pos, BlockState state) {
+        if (state.getBlock() instanceof ButtonBlock button) {
+            if (!button.defaultBlockState().getValue(BlockStateProperties.POWERED)) {
+                button.press(state, level, pos);
+
+                SoundEvent sound;
+                if (state.is(BlockTags.WOODEN_BUTTONS)) {
+                    sound = SoundEvents.WOODEN_BUTTON_CLICK_ON;
+                } else if (state.is(USBlockTags.COPPER_BUTTONS)) {
+                    sound = USSoundEvents.COPPER_CLICK.get();
+                } else {
+                    sound = SoundEvents.STONE_BUTTON_CLICK_ON;
+                }
+
+                level.playSound(null, pos, sound, SoundSource.BLOCKS, 0.3F, 0.6F);
+            }
+        } else if (state.getBlock() instanceof LeverBlock lever) {
+            lever.pull(state, level, pos);
+            level.playSound(null, pos, SoundEvents.LEVER_CLICK, SoundSource.BLOCKS, 0.3F, 0.6F);
+        }
+    }
+
+    private boolean canInteractWithPosition(CopperGolem golem) {
+        return golem.getBrain().getMemory(USMemoryModules.INTERACTION_POS.get()).isPresent() &&
+                golem.getBrain().getMemory(USMemoryModules.INTERACTION_COOLDOWN_TICKS.get()).isEmpty();
     }
 }
